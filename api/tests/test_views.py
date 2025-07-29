b655fe64d8ea0f99782736a91abc80034e876524
Cadu Menezes <2023252@student.cct.ie>
2025-07-29 20:20:19 +0100

    fix(test_views.py)= Updating workflow

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -7,19 +7,19 @@ class BookViewTest(APITestCase):
     
     def test_response_is_correct(self):
         book = Book.objects.create(
-        title="Demo",
-        description="Description",
+            title="Demo",
+            description="Description",
             author="Author"
-    )
+        )
 
-    url = reverse('api:books')
-    response = self.client.get(url, format='json')
-    assert response.status_code == status.HTTP_200_OK
-    body = response.json()
-    returned_book = body[0]
-    assert returned_book["title"] == book.title
-    assert returned_book["description"] == book.description
-    assert returned_book["author"] == book.author
+        url = reverse('api:books')
+        response = self.client.get(url, format='json')
+        assert response.status_code == status.HTTP_200_OK
+        body = response.json()
+        returned_book = body[0]
+        assert returned_book["title"] == book.title
+        assert returned_book["description"] == book.description
+        assert returned_book["author"] == book.author
 
 class HealthViewTest(APITestCase):
     
